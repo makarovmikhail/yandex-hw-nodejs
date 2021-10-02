5f9d6abdd4d8e2a20810a96597ea8ff1cf20be16
Mikhail Makarov <mikhailmakarov@MacBook-Pro-Mikhail.local>
2021-10-02 21:24:41 +0300

    update server

--- index.js
+++ index.js
@@ -7,89 +7,88 @@ const db = require("./src/entities/Database");
 const backrem = require("backrem");
 const fs = require("fs");
 
-// const MyCustomStorage = require("./src/utils/multerStorage");
-// const storage = new MyCustomStorage({
-//   destination: function (req, file, cb) {
-//     const imageId = generateId();
-//     file.imageId = imageId;
-//     cb(
-//       null,
-//       path.join(__dirname, "./images", new File(imageId).getOriginalFileName())
-//     );
-//   }
-// });
-// const upload = multer({storage});
+const MyCustomStorage = require("./src/utils/multerStorage");
+const storage = new MyCustomStorage({
+  destination: function (req, file, cb) {
+    const imageId = generateId();
+    file.imageId = imageId;
+    cb(
+      null,
+      path.join(__dirname, "./images", new File(imageId).getOriginalFileName())
+    );
+  }
+});
+const upload = multer({storage});
 
 const {PORT} = require("./src/config");
 const {removeFile} = require("./src/utils/fs");
 
 const app = express();
 
-// app.post("/upload", upload.single("image"), (req, res) => {
-//   res.json(req.file.imageId);
-// });
-
-app.get("/list", (req, res) => {
-  res.json([]);
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.json(req.file.imageId);
 });
 
-// app.get("/image/:id", (req, res) => {
-//   res.json({imageId: req.params.id});
-// });
+app.get("/list", (req, res) => {
+  res.json(db.images);
+});
 
-// app.delete("/image/:id", (req, res) => {
-//   const imageId = req.params.id;
-//   const image = db.findOne(imageId);
-//   if (!image) {
-//     res.json({stutus: "error"});
-//   } else {
-//     removeFile(path.join(__dirname, "./images", image.getOriginalFileName())).then(
-//       (result) => {
-//         db.remove(imageId);
-//         res.json({status: "ok"});
-//       },
-//       (err) => {
-//         res.json({stutus: "error"});
-//       }
-//     );
-//   }
-// });
+app.get("/image/:id", (req, res) => {
+  res.json({imageId: req.params.id});
+});
 
-// app.get("/merge", (req, res) => {
-//   const front = req.query.front;
-//   const back = req.query.back;
-//   const color = JSON.parse(req.query.color);
-//   const threshold = Number(req.query.threshold);
-//   console.log(color.length);
+app.delete("/image/:id", (req, res) => {
+  const imageId = req.params.id;
+  const image = db.findOne(imageId);
+  if (!image) {
+    res.json({stutus: "error"});
+  } else {
+    removeFile(path.join(__dirname, "./images", image.getOriginalFileName())).then(
+      (result) => {
+        db.remove(imageId);
+        res.json({status: "ok"});
+      },
+      (err) => {
+        res.json({stutus: "error"});
+      }
+    );
+  }
+});
 
-//   const frontImage = db.findOne(front);
-//   const backImage = db.findOne(back);
+app.get("/merge", (req, res) => {
+  const front = req.query.front;
+  const back = req.query.back;
+  const color = JSON.parse(req.query.color);
+  const threshold = Number(req.query.threshold);
 
-//   const frontImageFile = fs.createReadStream(
-//     path.join(__dirname, "./target/cat.jpeg")
-//   );
+  const frontImage = db.findOne(front);
+  const backImage = db.findOne(back);
 
-//   const backImageFile = fs.createReadStream(
-//     path.join(__dirname, "./target/space.jpeg")
-//   );
+  const frontImageFile = fs.createReadStream(
+    path.join(__dirname, "./target/cat.jpeg")
+  );
 
-//   backrem
-//     .replaceBackground(frontImageFile, backImageFile, color, threshold)
-//     .then(async (readableStream) => {
-//       const writableStream = fs.createWriteStream(
-//         path.resolve(__dirname, "./images/result.jpg")
-//       );
-//       readableStream
-//         .pipe(writableStream)
-//         .on("error", (err) => {
-//           console.log(err);
-//           res.json({status: err});
-//         })
-//         .on("finish", () => {
-//           res.json({status: "OK"});
-//         });
-//     });
-// });
+  const backImageFile = fs.createReadStream(
+    path.join(__dirname, "./target/space.jpeg")
+  );
+
+  backrem
+    .replaceBackground(frontImageFile, backImageFile, color, threshold)
+    .then(async (readableStream) => {
+      const writableStream = fs.createWriteStream(
+        path.resolve(__dirname, "./images/result.jpg")
+      );
+      readableStream
+        .pipe(writableStream)
+        .on("error", (err) => {
+          console.log(err);
+          res.json({status: err});
+        })
+        .on("finish", () => {
+          res.json({status: "OK"});
+        });
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
